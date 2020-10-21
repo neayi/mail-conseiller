d1994a7a27cbc0a5c228e26e34b7ae82dac638e6
Bertrand Gorge <bertrand.gorge@neayi.com>
2020-10-21 17:40:05 +0200

    Initial commit

---
+++ public_html/index.php
@@ -0,0 +1,244 @@
+<?php
+
+$pratiques = array();
+
+$pratiques[1]['author'] = "Georges Joya";
+$pratiques[1]['caption'] = "Alternance des cultures pour lutter contre les graminées d’hiver - retour d'expérience (Georges Joya - Aglae)";
+$pratiques[1]['emailCaption'] = "Alternance des cultures pour lutter contre les graminées d’hiver";
+$pratiques[1]['url'] = 'https://wiki.tripleperformance.fr/wiki/Alternance_des_cultures_pour_lutter_contre_les_gramin%C3%A9es_d%E2%80%99hiver_-_retour_d%27exp%C3%A9rience_(Georges_Joya_-_Aglae)';
+$pratiques[2]['author'] = "Guy Doléac";
+$pratiques[2]['caption'] = "Choix variétal pour limiter le développement des adventices en maïs ensilage - retour d'expérience (Guy Doléac - Aglae)";
+$pratiques[2]['emailCaption'] = "Choix variétal pour limiter le développement des adventices en maïs ensilage";
+$pratiques[2]['url'] = 'https://wiki.tripleperformance.fr/wiki/Choix_vari%C3%A9tal_pour_limiter_le_d%C3%A9veloppement_des_adventices_en_ma%C3%AFs_ensilage_-_retour_d%27exp%C3%A9rience_(Guy_Dol%C3%A9ac_-_Aglae)';
+$pratiques[3]['author'] = "Alain Marty";
+$pratiques[3]['caption'] = "Combinaison du désherbage mécanique et chimique des cultures de printemps - retour d'expérience (Alain Marty - Aglae)";
+$pratiques[3]['emailCaption'] = "Combinaison du désherbage mécanique et chimique des cultures de printemps";
+$pratiques[3]['url'] = 'https://wiki.tripleperformance.fr/wiki/Combinaison_du_d%C3%A9sherbage_m%C3%A9canique_et_chimique_des_cultures_de_printemps_-_retour_d%27exp%C3%A9rience_(Alain_Marty_-_Aglae)';
+$pratiques[4]['author'] = "Robert Melix";
+$pratiques[4]['caption'] = "Ecimeuse pour désherber dans les légumineuses à graines - retour d'expérience en bio (Robert Melix - Aglae)";
+$pratiques[4]['emailCaption'] = "Ecimeuse pour désherber dans les légumineuses à graines en bio";
+$pratiques[4]['url'] = 'https://wiki.tripleperformance.fr/wiki/Ecimeuse_pour_d%C3%A9sherber_dans_les_l%C3%A9gumineuses_%C3%A0_graines_-_retour_d%27exp%C3%A9rience_en_bio_(Robert_Melix_-_Aglae)';
+$pratiques[5]['author'] = "Jean-Michel Bardou";
+$pratiques[5]['caption'] = "Féverole en couvert végétal d'interculture - retour d'expérience (Jean-Michel Bardou - Aglae)";
+$pratiques[5]['emailCaption'] = "Féverole en couvert végétal d'interculture";
+$pratiques[5]['url'] = 'https://wiki.tripleperformance.fr/wiki/F%C3%A9verole_en_couvert_v%C3%A9g%C3%A9tal_d%27interculture_-_retour_d%27exp%C3%A9rience_(Jean-Michel_Bardou_-_Aglae)';
+$pratiques[6]['author'] = "Joël Coureau";
+$pratiques[6]['caption'] = "Fauchage - andainage multi-espèces : une solution pour les cultures à récolte compliquée - retour d'expérience (Joël Coureau - Aglae)";
+$pratiques[6]['emailCaption'] = "Fauchage - andainage multi-espèces : une solution pour les cultures à récolte compliquée";
+$pratiques[6]['url'] = 'https://wiki.tripleperformance.fr/wiki/Fauchage_-_andainage_multi-esp%C3%A8ces_:_une_solution_pour_les_cultures_%C3%A0_r%C3%A9colte_compliqu%C3%A9e_-_retour_d%27exp%C3%A9rience_(Jo%C3%ABl_Coureau_-_Aglae)';
+$pratiques[7]['author'] = "Damien Carpene";
+$pratiques[7]['caption'] = "Faux-semis scalpeur pour lutter contre les chardons en désherbage mécanique - retour d'expérience (Damien Carpene - Aglae)";
+$pratiques[7]['emailCaption'] = "Faux-semis scalpeur pour lutter contre les chardons en désherbage mécanique";
+$pratiques[7]['url'] = 'https://wiki.tripleperformance.fr/wiki/Faux-semis_scalpeur_pour_lutter_contre_les_chardons_en_d%C3%A9sherbage_m%C3%A9canique_-_retour_d%27exp%C3%A9rience_(Damien_Carpene_-_Aglae)';
+$pratiques[8]['author'] = "Jean-Jacques Garbay";
+$pratiques[8]['caption'] = "Gestion de la flore en colza bio post couvert végétal - retour d'expérience (Jean-Jacques Garbay - Aglae)";
+$pratiques[8]['emailCaption'] = "Gestion de la flore en colza bio post couvert végétal";
+$pratiques[8]['url'] = 'https://wiki.tripleperformance.fr/wiki/Gestion_de_la_flore_en_colza_bio_post_couvert_v%C3%A9g%C3%A9tal_-_retour_d%27exp%C3%A9rience_(Jean-Jacques_Garbay_-_Aglae)';
+$pratiques[9]['author'] = "Brice Bousquet";
+$pratiques[9]['caption'] = "Introduction de cultures fourragères dans une rotation céréalières avec ray-grass resistant - retour d'expérience (Brice Bousquet - Aglae)";
+$pratiques[9]['emailCaption'] = "Introduction de cultures fourragères dans une rotation céréalières avec ray-grass resistant";
+$pratiques[9]['url'] = 'https://wiki.tripleperformance.fr/wiki/Introduction_de_cultures_fourrag%C3%A8res_dans_une_rotation_c%C3%A9r%C3%A9ali%C3%A8res_avec_ray-grass_resistant_-_retour_d%27exp%C3%A9rience_(Brice_Bousquet_-_Aglae)';
+$pratiques[10]['author'] = "Chateau de Nages";
+$pratiques[10]['caption'] = "Paillage tissé sur le rang en vergers zéro résidus pesticides";
+$pratiques[10]['emailCaption'] = "Paillage tissé sur le rang en vergers zéro résidus pesticides";
+$pratiques[10]['url'] = 'https://wiki.tripleperformance.fr/wiki/Paillage_tiss%C3%A9_sur_le_rang_en_vergers_z%C3%A9ro_r%C3%A9sidus_pesticides';
+$pratiques[11]['author'] = "Jérôme Sainte-Marie";
+$pratiques[11]['caption'] = "Réduction de l'écartement et augmentation de la densité de semis pour réduire la pression adventice - retour d'expérience (Jérôme Sainte-Marie - Aglae)";
+$pratiques[11]['emailCaption'] = "Réduction de l'écartement et augmentation de la densité de semis pour réduire la pression adventice";
+$pratiques[11]['url'] = 'https://wiki.tripleperformance.fr/wiki/R%C3%A9duction_de_l%27%C3%A9cartement_et_augmentation_de_la_densit%C3%A9_de_semis_pour_r%C3%A9duire_la_pression_adventice_-_retour_d%27exp%C3%A9rience_(J%C3%A9r%C3%B4me_Sainte-Marie_-_Aglae)';
+
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Aglaé - Chambre d'Agriculture Occitanie</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <style>
+ .prepareMailButton {
+   float: right;
+   margin-bottom: 5rem;
+ }
+.form-check {
+  margin-bottom: 1rem;
+}
+  </style>
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <a class="navbar-brand" href="<?= $GLOBALS['_SERVER']['PHP_SELF'] ?>">Aglaé</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a target="_blank" class="nav-link" href="https://occitanie.chambre-agriculture.fr/agroenvironnement/agroecologie/aglae/">Chambre d'Agriculture d'Occitanie
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a target="_blank" class="nav-link" href="https://tripleperformance.fr">Triple Performance</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 mx-auto">
+
+<?php
+
+if (!empty($_GET['p']))
+{
+  if (!empty($_GET['nom']))
+    $nom = $_GET['nom'];
+  else
+    $nom = 'anonyme';
+
+  $nom = preg_replace("/[^a-zA-Z0-9]/", "", $nom);
+
+  foreach ($pratiques as $k => $p)
+    $pratiques[$k]['url'] = $p['url'] . "?utm_source=aglae&utm_medium=e-mail&utm_campaign=" . urlencode($nom);
+
+?>
+
+<div class="my-5">
+Votre mail est prêt ! Vous pouvez copier le texte ci-dessous et le coller dans votre outil de mail.
+N'hésitez pas à le modifier et à l'adapter si nécessaire !
+</div>
+
+<div class="card my-5">
+  <div class="card-body">
+
+
+<?php
+    if (count($_GET['p']) == 1)
+    {
+      $p = array_keys($_GET['p']);
+      $p = $pratiques[reset($p)];
+?>
+    Bonjour,<br><br>
+
+    J'espère que tout va bien !<br><br>
+
+    J'ai pensé à vous, car nous venons de finir la rédaction d'un nouveau retour d'expérience dans le cadre du programme Aglaé (avec la Chambre d'Agri Occitanie),
+    qui pourrait vous intéresser.<br><br>
+
+    Il s'agit d'un retour d'expérience de <?=$p['author'] ?> : <b><a href="<?=$p['url'] ?>"><?=$p['emailCaption'] ?></a></b>.<br><br>
+
+    La plateforme sur laquelle nous avons rédigé ce retour d'expérience (<a href="https://tripleperformance.fr">tripleperformance.fr</a>) permet en outre
+    de poser des questions et commentaires.  N'hésitez pas car cela permettra d'améliorer les témoignages, et, le cas échéant, de mettre les uns et les autres
+    en relation autour de chaque sujet.<br><br>
+
+    En vous souhaitant une très bonne journée,<br><br>
+
+    Cordialement,<br><br>
+
+  <?= $_GET['nom']; ?>
+
+<?php
+    }
+    else
+    {
+      $p = array_keys($_GET['p']);
+
+      ?>
+      Bonjour,<br><br>
+
+      J'espère que tout va bien !<br><br>
+
+      J'ai pensé à vous, car nous venons de finir la rédaction de nouveaux retours d'expérience dans le cadre du programme Aglaé (avec la Chambre d'Agri Occitanie),
+      qui pourraient vous intéresser.<br><br>
+
+      <ul>
+        <?php
+        foreach ($p as $k)
+          echo '<li><b><a href="'. $pratiques[$k]['url'] . '">'. $pratiques[$k]['emailCaption'] . '</a></b></li>';
+        ?>
+      </ul>
+      La plateforme sur laquelle nous avons rédigé ce retour d'expérience (<a href="https://tripleperformance.fr">tripleperformance.fr</a>) permet en outre
+      de poser des questions et commentaires.  N'hésitez pas car cela permettra d'améliorer les témoignages, et, le cas échéant, de mettre les uns et les autres
+      en relation autour de chaque sujet.<br><br>
+
+      En vous souhaitant une très bonne journée,<br><br>
+
+      Cordialement,<br><br>
+
+    <?= $_GET['nom']; ?>
+
+  <?php
+    }
+
+
+?>
+  </div>
+</div>
+
+
+
+<?php
+}
+else
+{
+
+?>
+
+        <h1 class="mt-5">Préparation du mail pour les agriculteurs</h1>
+        <p class="lead">Cette page permet de générer un mail à destination des agriculteurs que vous suivez, pour leur transmettre l'information sur le programme Aglaé</p>
+
+        <form method="get" action="">
+        <div>
+          <label for="nom"><b>Veuillez saisir votre nom : </b></label>
+          <input type="text" name="nom" id="nom" required>
+        </div>
+
+        <b>Et choisir la ou les pratiques que vous souhaitez partager avec votre contact :</b>
+<?php
+
+foreach ($pratiques as $k => $aPratique)
+{
+  echo '<div class="form-check">
+  <input type="checkbox" class="form-check-input" id="pratiqueId'.$k.'" name="p['.$k.']" value="yes">
+  <label class="form-check-label" for="pratiqueId'.$k.'">'.htmlentities($aPratique['caption']).'
+  <a target="_blank" href="'.htmlentities($aPratique['url']).'"><img src="https://wiki.tripleperformance.fr/skins/skin-neayi/images/external-link-ltr-icon.svg"></a>
+
+  </label>
+  </div>';
+}
+?>
+
+          <button type="submit" class="btn btn-primary prepareMailButton">Préparer le mail !</button>
+
+
+        </form>
+<?php
+}
+?>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.slim.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
